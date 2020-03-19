88561d3776cb6317bb534f46131ac7cb5ff32c90
SubramanyahPiktorlabs <subramanyaadiga17@gmail.com>
2020-03-19 13:06:51 +0530

    Undo last commit

--- app/src/main/java/com/piktorlabs/textrecognizer/MainActivity.java
+++ app/src/main/java/com/piktorlabs/textrecognizer/MainActivity.java
@@ -39,9 +39,9 @@ public class MainActivity extends AppCompatActivity {
     }
     public void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//        }
+        }
     }
 
     @Override
