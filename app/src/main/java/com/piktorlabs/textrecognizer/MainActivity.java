c9ca227b208735a5e9716a9274c5717114d31565
SubramanyahPiktorlabs <subramanyaadiga17@gmail.com>
2020-03-19 08:53:06 +0530

    dispatchTakePictureIntent method made Public

--- app/src/main/java/com/piktorlabs/textrecognizer/MainActivity.java
+++ app/src/main/java/com/piktorlabs/textrecognizer/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void dispatchTakePictureIntent() {
+    public void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
