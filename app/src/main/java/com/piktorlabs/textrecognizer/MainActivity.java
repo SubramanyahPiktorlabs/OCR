6c308a019eda9166d7b87e6f8fbcaf6674f64267
SubramanyahPiktorlabs <subramanyaadiga17@gmail.com>
2020-03-19 07:53:51 +0530

    added camera functionality to take picure

--- app/src/main/java/com/piktorlabs/textrecognizer/MainActivity.java
+++ app/src/main/java/com/piktorlabs/textrecognizer/MainActivity.java
@@ -2,13 +2,56 @@ package com.piktorlabs.textrecognizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button captureImage, selectImage, detectText;
+    private TextView detectedText;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureImage = findViewById(R.id.capture_image);
+        detectText = findViewById(R.id.detect_text);
+        imageView = findViewById(R.id.image_view);
+        detectedText = findViewById(R.id.image_text);
+
+        captureImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+                detectedText.setText("");
+            }
+        });
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
