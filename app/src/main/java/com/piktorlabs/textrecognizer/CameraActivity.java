e9893a53fa89cf73f871397d06cedf488b77e2ea
SubramanyahPiktorlabs <subramanyaadiga17@gmail.com>
2020-03-19 13:51:00 +0530

    Added New Activity Class

---
+++ app/src/main/java/com/piktorlabs/textrecognizer/CameraActivity.java
@@ -0,0 +1,13 @@
+package com.piktorlabs.textrecognizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity{
+    public String dispatchTakePictureIntent(String msg) {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+        return "Hello";
+    }
+}
